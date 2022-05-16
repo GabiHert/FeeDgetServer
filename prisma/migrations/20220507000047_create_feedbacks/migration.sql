d0f9f01cc97054804663205a61b5a48bfe9ec2ef
Gabriel Herter <gabriel.herter@dock.tech>
2022-05-16 18:11:05 -0300

    alterado provider para postgresql

--- prisma/migrations/20220507000047_create_feedbacks/migration.sql
+++ prisma/migrations/20220507000047_create_feedbacks/migration.sql
@@ -1,7 +1,1 @@
--- CreateTable
-CREATE TABLE "feedbacks" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "type" TEXT NOT NULL,
-    "comment" TEXT NOT NULL,
-    "screenshot" TEXT
-);
+provider = "postgresql"
